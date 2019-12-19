48cde7f272dc71e5f929d0df249ac5b87584a7b6
AndreNussbaumer <57812689+AndreNussbaumer@users.noreply.github.com>
2019-12-19 17:59:29 +0000

    Add files via upload

--- DBConnection.js
+++ DBConnection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 
+
 function getConnection(){
     var con = mysql.createConnection({
-        host:"localhost",
-        user:"root",
-        password:"",
-        database:"thestory"
+        host:"eu-cdbr-west-02.cleardb.net",
+        user:"bc70d0f8152b8f",
+        password:"551789e3",
+        database:"heroku_4eaa0c"
     });
     return con;
 }
